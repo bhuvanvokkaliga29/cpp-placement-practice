0cf5bda08392aa6ed4e1b41a81ceff0d6eefceb9
Bhuvan Gowda H K <hkbhuvan49@gmail.com>
2026-02-07 14:01:29 +0530

    Added second largest element program in C++

---
+++ second_largest.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int largest = -1e9, second = -1e9;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            second = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > second && arr[i] != largest)
+        {
+            second = arr[i];
+        }
+    }
+
+    if (second == -1e9)
+        cout << "No second largest";
+    else
+        cout << second;
+
+    return 0;
+}
