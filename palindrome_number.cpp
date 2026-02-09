26d2025ab9c377dcfff53136940baf22127bf32b
Bhuvan Gowda H K <hkbhuvan49@gmail.com>
2026-02-10 00:27:49 +0530

    Added palindrome number program in C++

---
+++ palindrome_number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int temp = n, rev = 0;
+    while (n > 0)
+    {
+        rev = rev * 10 + (n % 10);
+        n /= 10;
+    }
+
+    if (temp == rev)
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+    return 0;
+}
