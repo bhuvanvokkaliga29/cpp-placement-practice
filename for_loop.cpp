a91dd62656e67d6fa4c6cdf145ceb3beb6e31f54
Bhuvan Gowda H K <hkbhuvan49@gmail.com>
2026-02-13 15:23:15 +0530

    Added for loop statement program

---
+++ for_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Numbers from 1 to 10:" << endl;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
