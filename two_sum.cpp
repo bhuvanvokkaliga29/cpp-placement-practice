421dc3fa22abfab6b7f5adacdb911e3945670b15
Bhuvan Gowda H K <hkbhuvan49@gmail.com>
2026-02-18 15:22:46 +0530

    Added Two Sum brute force solution in C++

---
+++ two_sum.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, target;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter target: ";
+    cin >> target;
+
+    bool found = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] + arr[j] == target)
+            {
+                cout << "Indices: " << i << " " << j;
+                found = true;
+                break;
+            }
+        }
+        if (found)
+            break;
+    }
+
+    if (!found)
+    {
+        cout << "No pair found";
+    }
+
+    return 0;
+}
